c25a3051fabb27e47fb3e3b84dc3e6e41b9b9d0d
habeshe <yfreheiwot@gmail.com>
2026-03-23 18:56:36 +0300

    Create Reverse Linked List.py

---
+++ Reverse Linked List.py
@@ -0,0 +1,8 @@
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node=None
+        while head:
+            temp=head.next
+            head.next=node
+            node=head
+            head=temp
+        return node
