02cf66f1ce863604aaefd4848036c84e0ab49f74
habeshe <yfreheiwot@gmail.com>
2026-03-30 18:52:50 +0300

    Create Letter Combinations of a Phone Number.py

---
+++ Letter Combinations of a Phone Number.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hash={"2":["a","b","c"],"3":["d","e","f"],"4":["g","h","i"],"5":["j","k","l"],"6":["m","n","o"],"7":["p","q","r","s"],"8":["t","u","v"],"9":["w","x","y","z"]}
+        d=[hash[d] for d in digits]
+        com=product(*d)
+        return ["".join(c) for c in com]
+        
+
+
+
+        
