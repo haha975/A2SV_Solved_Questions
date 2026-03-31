a4cc516c9df2311f07e9ee3d7d793dcab8cade07
habeshe <yfreheiwot@gmail.com>
2026-03-31 14:55:35 +0300

    Add solution for 132 Pattern

---
+++ leetcode/0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack=[]
+        minn=nums[0]
+        for n in nums[1:]:
+            while stack and stack[-1][0]<=n:
+                stack.pop()
+            if stack and n>stack[-1][1]:
+                return True
+            stack.append([n,minn])
+            minn=min(minn,n)
+        return False
