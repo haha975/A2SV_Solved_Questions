929b9429719e262aa8b9c987df56ee631d14be9d
habeshe <yfreheiwot@gmail.com>
2026-04-19 10:49:50 +0300

    Add solution for Two Sum

--- leetcode/0001-two-sum/0001-two-sum.py
+++ leetcode/0001-two-sum/0001-two-sum.py
@@ -2,38 +2,34 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash={}
         for i,k in enumerate(nums):
+            dif=target-k
+            
+            if dif in hash:
+                return [hash[dif][0],i]
             if k not in hash:
                 hash[k]=[]
             hash[k].append(i)
         
-        nums.sort()
+        # nums.sort()
 
-        left=0
-        right=len(nums)-1
+        # left=0
+        # right=len(nums)-1
         
-        while left<right:
-            su=nums[left]+nums[right]
-            if su==target:
-                c=hash[nums[left]][0]
-                hash[nums[left]].pop(0)
-                d=hash[nums[right]][0]
-                return [c,d]
-            elif su>target:
-                right-=1
-            elif su<target:
-                left+=1 
+        # while left<right:
+        #     su=nums[left]+nums[right]
+        #     if su==target:
+        #         c=hash[nums[left]][0]
+        #         hash[nums[left]].pop(0)
+        #         d=hash[nums[right]][0]
+        #         return [c,d]
+        #     elif su>target:
+        #         right-=1
+        #     elif su<target:
+        #         left+=1 
 
 
     
-        # k=[]
-        # for i in range(len(nums)):
-        #     for j in range(len(nums)):
-        #         if (nums[i]+nums[j])==target and i<j:
-        #             k.append(i)
-        #             k.append(j)
-                    
-                
-        # return k
+      
 
         
         
